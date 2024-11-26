c7311eec2d0ca1551e6c5a2a1ff48d0f435a0465
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2024-11-26 19:18:11 +0100

    Things
    I don't have time to split efficiently when I'm writing code for myself

--- bin/commands.ml
+++ bin/commands.ml
@@ -2,11 +2,13 @@ let socket_name = ".socket.sock"
 
 type monitor = { id : int; name : string; desc : string }
 type workspace = Wksp of int
+type move_workspace = { workspace : workspace; monitor : monitor }
 
 type _ command =
   | Monitors : monitor list command
   | Workspaces : workspace list command
-  | Seq : 'a command * 'b command -> ('a * 'b) command
+  | MoveWorkspaceToMonitor : move_workspace -> bool command
+(* | Seq : 'a command * 'b command -> ('a * 'b) command *)
 
 module Option_syntax = struct
   let ( let* ) x f = Option.bind x f
@@ -49,41 +51,47 @@ let parse_workspaces = function
   | `A workspace -> Some (List.filter_map parse_workspace workspace)
   | _ -> None
 
-let rec pp_command : type t. _ -> t command * t -> unit =
+let pp_command : type t. _ -> t command * t -> unit =
  fun ppf -> function
   | Monitors, monitors -> pp_monitors ppf monitors
   | Workspaces, workspaces -> pp_workspaces ppf workspaces
-  | Seq (c1, c2), (res1, res2) ->
-      Format.fprintf ppf "%a; %a" pp_command (c1, res1) pp_command (c2, res2)
+  | MoveWorkspaceToMonitor { workspace; monitor }, b ->
+      Format.fprintf ppf "Move %a to %a: %b" pp_workspace workspace pp_monitor
+        monitor b
+(* | Seq (c1, c2), (res1, res2) -> *)
+(*     Format.fprintf ppf "%a; %a" pp_command (c1, res1) pp_command (c2, res2) *)
 
 (* let merge_array_parsers p1 p2 = function `A [ r1; r2 ] -> p1 * p2 | `A [ *)
 
-let rec prepare_command :
-    type res. res command -> string * (Ezjsonm.value -> res option) =
-  let open Option_syntax in
+let as_json k v = k (Ezjsonm.from_string v)
+
+let prepare_command : type res. res command -> string * (string -> res option) =
   function
-  | Monitors -> ("j/monitors", parse_monitors)
-  | Workspaces -> ("j/workspaces", parse_workspaces)
-  | Seq (c1, (Seq (_, _) as subseq)) -> (
-      let c1_dispatch, c1_parser = prepare_command c1 in
-      let subseq_dispatch, subseq_parser = prepare_command subseq in
-      ( "[[BATCH]]" ^ c1_dispatch ^ ";" ^ subseq_dispatch,
-        function
-        | `A (v1 :: (_ :: _ :: _ as subarray)) ->
-            let* r1 = c1_parser v1 in
-            let* r2 = subseq_parser (`A subarray) in
-            Some (r1, r2)
-        | _ -> None ))
-  | Seq (c1, c2) -> (
-      let c1_dispatch, c1_parser = prepare_command c1 in
-      let c2_dispatch, c2_parser = prepare_command c2 in
-      ( c1_dispatch ^ ";" ^ c2_dispatch,
-        function
-        | `A [ v1; v2 ] ->
-            let* r1 = c1_parser v1 in
-            let* r2 = c2_parser v2 in
-            Some (r1, r2)
-        | _ -> None ))
+  | Monitors -> ("j/monitors", as_json parse_monitors)
+  | Workspaces -> ("j/workspaces", as_json parse_workspaces)
+  | MoveWorkspaceToMonitor { workspace = Wksp id; monitor; _ } ->
+      ( Format.sprintf "j/dispatch moveworkspacetomonitor %d %s" id monitor.name,
+        fun r -> Some (r = "ok") )
+(* | Seq (c1, (Seq (_, _) as subseq)) -> ( *)
+(*     let c1_dispatch, c1_parser = prepare_command c1 in *)
+(*     let subseq_dispatch, subseq_parser = prepare_command subseq in *)
+(*     ( "[[BATCH]]" ^ c1_dispatch ^ ";" ^ subseq_dispatch, *)
+(*       function *)
+(*       | `A (v1 :: (_ :: _ :: _ as subarray)) -> *)
+(*           let* r1 = c1_parser v1 in *)
+(*           let* r2 = subseq_parser (`A subarray) in *)
+(*           Some (r1, r2) *)
+(*       | _ -> None )) *)
+(* | Seq (c1, c2) -> ( *)
+(*     let c1_dispatch, c1_parser = prepare_command c1 in *)
+(*     let c2_dispatch, c2_parser = prepare_command c2 in *)
+(*     ( c1_dispatch ^ ";" ^ c2_dispatch, *)
+(*       function *)
+(*       | `A [ v1; v2 ] -> *)
+(*           let* r1 = c1_parser v1 in *)
+(*           let* r2 = c2_parser v2 in *)
+(*           Some (r1, r2) *)
+(*       | _ -> None )) *)
 
 let send_command_raw :
     type cmd_res.
@@ -93,7 +101,7 @@ let send_command_raw :
   let dispatch, parse = prepare_command command in
   Eio.traceln "Dispatch: %s" dispatch;
   try
-    let* result = Socket.request_json dispatch_socket dispatch parse in
+    let* result = Socket.request dispatch_socket dispatch parse in
     Eio.traceln "Result: [%a]" pp_command (command, result);
     Some result
   with _exn -> None
--- bin/handler.ml
+++ bin/handler.ml
@@ -1,11 +1,40 @@
+let dispatch_workspaces ?ev ~sw ~env () =
+  let open Commands.Option_syntax in
+  (* This is for debug only *)
+  Option.iter
+    (fun ev ->
+      Eio.traceln "Monitor changed: %s >> %s"
+        (Event.to_string ev.Event.event)
+        ev.data)
+    ev;
+  let monitors, workspaces =
+    Eio.Fiber.pair
+      (fun () -> Commands.send_command ~sw ~env Commands.Monitors)
+      (fun () -> Commands.send_command ~sw ~env Commands.Workspaces)
+  in
+  let* monitors = monitors in
+  let* workspaces = workspaces in
+  let workspaces = Layout.assign monitors workspaces in
+  let commands =
+    List.filter_map
+      (fun assignment ->
+        if assignment.Layout.active then
+          Some
+            (fun () ->
+              Commands.send_command ~sw ~env
+                (Commands.MoveWorkspaceToMonitor
+                   { workspace = assignment.id; monitor = assignment.monitor })
+              |> ignore)
+        else None)
+      workspaces
+  in
+  let () = Eio.Fiber.all commands in
+  Some ()
+
+let on_monitor_change ?ev sw env = dispatch_workspaces ~sw ~env ?ev ()
+
 let on_event sw env ev =
   match ev.Event.event with
   | Event.MonitorAdded | MonitorAddedV2 | MonitorRemoved ->
-      Eio.traceln "Monitor changed: %s >> %s" (Event.to_string ev.event) ev.data;
-      let _monitors, _workspaces =
-        Eio.Fiber.pair
-          (fun () -> Commands.send_command ~sw ~env Commands.Monitors)
-          (fun () -> Commands.send_command ~sw ~env Commands.Workspaces)
-      in
-      ()
+      on_monitor_change sw env ~ev |> ignore
   | _ -> ()
