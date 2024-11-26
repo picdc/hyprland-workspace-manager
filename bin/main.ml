829b02c99a456ec3aadb31edcd7b0ff3b83d705b
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2024-11-26 19:17:59 +0100

    Use cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,7 @@
-let read_socket ~sw ~env event_socket =
+open Cmdliner
+
+let daemon ~sw ~env event_socket =
+  Eio.traceln "Start daemon";
   let buf = Eio.Buf_read.of_flow ~max_size:max_int event_socket in
   while true do
     let msg = Eio.Buf_read.line buf in
@@ -7,7 +10,24 @@ let read_socket ~sw ~env event_socket =
     | None -> ()
   done
 
+module Daemon = struct
+  let handler ~sw ~env () =
+    Socket.with_connection ~sw ~env Event.socket_name daemon
+
+  let term ~sw ~env = Term.(const (handler ~sw ~env) $ const ())
+  let cmd ~sw ~env = Cmd.v (Cmd.info "daemon") (term ~sw ~env)
+end
+
+module Dispatch = struct
+  let handler ~sw ~env () = Handler.dispatch_workspaces ~sw ~env () |> ignore
+  let term ~sw ~env = Term.(const (handler ~sw ~env) $ const ())
+  let cmd ~sw ~env = Cmd.v (Cmd.info "dispatch") (term ~sw ~env)
+end
+
+let commands ~sw ~env =
+  Cmdliner.Cmd.group (Cmd.info "")
+    [ Daemon.cmd ~sw ~env; Dispatch.cmd ~sw ~env ]
+
 let () =
   Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
-  Socket.with_connection ~sw ~env Event.socket_name read_socket
+  Eio.Switch.run @@ fun sw -> exit (Cmd.eval (commands ~sw ~env))
